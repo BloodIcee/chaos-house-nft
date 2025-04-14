7bc03bdd0d7fdee7e8698136f77d9cca1caeddd3
BloodIce <nikolajzirko@gmail.com>
2025-04-14 16:53:43 +0300

    Import Assets pt.2

---
+++ Assets/Imports/FImpossible Creations/Plugins - Level Design/PGG/Core/Grid Fundamentals/FGenGrid.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+namespace FIMSpace.Generating
+{
+    public class FGenGrid<T> where T : class, new()
+    {
+        public FGenTwoDirDynamicList<FGenTwoDirDynamicList<FGenTwoDirDynamicList<T>>> Dimensions { get; private set; }
+        public delegate void OnGeneratedElement(T generated);
+
+        public FGenGrid()
+        {
+            Dimensions = new FGenTwoDirDynamicList<FGenTwoDirDynamicList<FGenTwoDirDynamicList<T>>>();
+        }
+
+        public T GetCell(float x, float y, float z, Action<T,int> callback = null, bool generateIfOut = true)
+        {
+            return Dimensions
+                .GetAt((int)x)
+                .GetAt((int)y)
+                .GetAt((int)z, callback, generateIfOut);
+        }
+
+        public T GetCell(float x, float z, Action<T, int> callback = null, bool generateIfOut = true)
+        {
+            return Dimensions.
+            GetAt((int)x)
+            .GetAt(0).
+            GetAt((int)z, callback, generateIfOut);
+        }
+
+        public void ReplaceCell(int x, int y, int z, T cell)
+        {
+            Dimensions.GetAt(x).GetAt(y).SetAt(z, cell);
+        }
+
+        public Vector2Int GetXSize()
+        {
+            return new Vector2Int(Dimensions.GetNegativeLength(), Dimensions.GetPositiveLength());
+        }
+
+        public Vector2Int GetYSize()
+        {
+            return new Vector2Int(Dimensions.GetAt(0).GetNegativeLength(), Dimensions.GetAt(0).GetPositiveLength());
+        }
+
+        public Vector2Int GetZSize()
+        {
+            return new Vector2Int(Dimensions.GetAt(0).GetAt(0).GetNegativeLength(), Dimensions.GetAt(0).GetAt(0).GetPositiveLength());
+        }
+
+        public void Clear()
+        {
+            Dimensions.Clear();
+        }
+    }
+}
